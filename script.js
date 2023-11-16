0316639c48a8f953f5b258dc31f8533c9a76d9ca
Abhishek Garg <103814691+AbhishekGarg0507@users.noreply.github.com>
2023-11-16 13:35:31 +0530

    update

--- script.js
+++ script.js
@@ -7,6 +7,45 @@ function setScrollVar(){
             htmlElement.scrollTop/ htmlElement.clientHeight
     htmlElement.style.setProperty("--scroll",
         Math.min(percentageOfScreenScrolled *100,100))   
+
+    htmlElement.style.setProperty("--scrollFull" , (percentageOfScreenScrolled.toFixed(1))*100 )   
+        
 }
 
 setScrollVar();
+
+const observer = new IntersectionObserver(entries =>{
+        for(let i = entries.length-1 ; i >=0 ; i--){
+                const entry = entries[i]
+                if(entry.isIntersecting){
+                        document.querySelectorAll("[data-img]").forEach(img =>{
+                                img.classList.remove("show")
+                        })
+                        const img = document.querySelector(entry.target.dataset.imgToShow)
+                        img?.classList.add("show")
+                        break
+                }
+        }
+        
+})
+
+document.querySelectorAll("[data-img-to-show]").forEach(section =>{
+        observer.observe(section);
+})
+const obse = new IntersectionObserver(entry =>{
+
+                if(entry.isIntersecting){
+                        document.querySelector("[data-img-last]").forEach(img =>{
+                                img.classList.remove("lastimg")
+                        })
+                        const img = document.querySelector(entry.target.dataset.imgToShow)
+                        img?.classList.add("lastimg")
+                }
+        
+        
+})
+
+document.querySelectorAll("[data-img-to-show]").forEach(section =>{
+        observer.observe(section);
+})
+
